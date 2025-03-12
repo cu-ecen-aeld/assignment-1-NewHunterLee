712a4d651c9b945141763d7685cf1d4851a753d8
lzzzz <166602503+NewHunterLee@users.noreply.github.com>
2025-03-13 02:58:10 +0800

    Update Test_validate_username.c

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -9,14 +9,16 @@
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
 *   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
 *       the assignment autotest submodule at assignment-autotest/test/assignment1/
-*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal.  See
-*       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
+*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal. 
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // 1) Get the hardcoded username from my_username()
+    const char *username_from_code = my_username();
+
+    // 2) Get the username from the configuration file using malloc_username_from_conf_file()
+    const char *username_from_file = malloc_username_from_conf_file("/conf/username.txt");
+
+    // 3) Verify that both usernames are equal using TEST_ASSERT_EQUAL_STRING_MESSAGE
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(username_from_code, username_from_file, "Usernames do not match!");
 }
